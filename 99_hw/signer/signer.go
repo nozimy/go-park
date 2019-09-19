6d9e8635877ba8592fe8d773a7c5763936fe5ec2
Nozim Yunusov <nozimdev@gmail.com>
2019-09-19 20:04:57 +0300

    refactor(dz2) clean code

--- 99_hw/signer/signer.go
+++ 99_hw/signer/signer.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"sort"
 	"strconv"
 	"strings"
@@ -21,26 +20,23 @@ func (s *single) get() string {
 func ExecutePipeline(jobs ...job) {
 	in := make(chan interface{})
 	out := make(chan interface{})
-	result := make(chan int)
+	end := make(chan int)
 
 	for i, doJob := range jobs {
 		in = out
 		out = make(chan interface{})
 
-		go func(in2, out2 chan interface{}, i2 int, result2 chan<- int, doJob2 job, lastIndex int) {
-			defer close(out2)
+		go func(inInner, outInner chan interface{}, index int, endInner chan<- int, doJobInner job, lastIndex int) {
+			defer close(outInner)
 
-			doJob2(in2, out2)
-
-			log.Println("done   doJob", i2)
-
-			if i2 == lastIndex {
-				result2 <- i2
+			doJobInner(inInner, outInner)
+			if index == lastIndex {
+				endInner <- index
 			}
-		}(in, out, i, result, doJob, len(jobs)-1)
+		}(in, out, i, end, doJob, len(jobs)-1)
 	}
 
-	<-result
+	<-end
 }
 
 var SingleHash = func(in, out chan interface{}) {
@@ -51,8 +47,7 @@ var SingleHash = func(in, out chan interface{}) {
 		crc32 := make(chan string)
 
 		go func() {
-			log.Println("DataSignerMd5")
-			md5 <- DataSignerMd5(data) //перегрев на 1 сек
+			md5 <- DataSignerMd5(data)
 		}()
 		go func() {
 			crc32Md5 <- DataSignerCrc32(<-md5)
@@ -75,45 +70,8 @@ var MultiHash = func(in, out chan interface{}) {
 
 	for i := range in {
 		data := i.(single)
-
 		wgOuter.Add(1)
-
-		go func(data2 single, out2 chan interface{}, waiterOuter *sync.WaitGroup) {
-			defer waiterOuter.Done()
-
-			log.Println("MultiHash Start")
-			d := <-data2.crc32 + "~" + <-data2.crc32Md5
-			log.Println("MultiHash after data extraction", d)
-
-			wg := &sync.WaitGroup{}
-			resultMap := make(map[int]string)
-			mu := &sync.Mutex{}
-			var result string
-
-			for i := 0; i < 6; i++ {
-				wg.Add(1)
-				go func(i2 int, waiter *sync.WaitGroup) {
-					defer waiter.Done()
-					th := strconv.Itoa(i2)
-
-					res := DataSignerCrc32(th + d)
-					mu.Lock()
-					resultMap[i2] = res
-					mu.Unlock()
-				}(i, wg)
-			}
-
-			wg.Wait()
-
-			for i := 0; i < len(resultMap); i++ {
-				result += resultMap[i]
-			}
-
-			log.Println("MultiHash Finish")
-
-			log.Println("MultiHash End")
-			out2 <- result
-		}(data, out, wgOuter)
+		go doMultiHash(data, out, wgOuter)
 	}
 
 	wgOuter.Wait()
@@ -131,4 +89,37 @@ var CombineResults = func(in, out chan interface{}) {
 	})
 
 	out <- strings.Join(data, "_")
+}
+
+var doDataSignerCrc32 = func(index int, data string, resultMap map[int]string, mu *sync.Mutex, waiter *sync.WaitGroup) {
+	defer waiter.Done()
+	th := strconv.Itoa(index)
+	resString := DataSignerCrc32(th + data)
+	mu.Lock()
+	resultMap[index] = resString
+	mu.Unlock()
+}
+
+var doMultiHash = func(data single, out chan interface{}, waiter *sync.WaitGroup) {
+	defer waiter.Done()
+
+	dataStr := data.get()
+
+	wg := &sync.WaitGroup{}
+	resultMap := make(map[int]string)
+	mu := &sync.Mutex{}
+	var result string
+
+	for i := 0; i < 6; i++ {
+		wg.Add(1)
+		go doDataSignerCrc32(i, dataStr, resultMap, mu, wg)
+	}
+
+	wg.Wait()
+
+	for i := 0; i < len(resultMap); i++ {
+		result += resultMap[i]
+	}
+
+	out <- result
 }
